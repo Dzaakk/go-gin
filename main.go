94e2d22b2ebe18a804bd203fb37722c60fa7f5aa
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-04-24 00:23:26 +0700

    rebuild

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ func main() {
 	router := gin.Default()
 	router.GET("/", rootHandler)
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 	router.Run() // if () port:8080
+
 }
 
 func rootHandler(c *gin.Context) {
@@ -24,4 +27,16 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title": "Hello",
 	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
 }
